641120d995da0a7b9070e2b2d6788a4d35542a95
Ranjeet Vishwakarma <ranjeetv.dev@gmail.com>
2026-04-04 11:36:54 +0530

    changed the variable name

--- app/subscriptions/[id].tsx
+++ app/subscriptions/[id].tsx
@@ -3,16 +3,16 @@ import { styled } from "nativewind";
 import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
-const SafeAreaVIew = styled(RNSafeAreaView);
+const SafeAreaView = styled(RNSafeAreaView);
 
 const SubscriptionDetails = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
 
   return (
-    <SafeAreaVIew className="flex-1 bg-background p-5">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text>Subscription details: {id}</Text>
       <Link href="/">Go back</Link>
-    </SafeAreaVIew>
+    </SafeAreaView>
   );
 };
 
